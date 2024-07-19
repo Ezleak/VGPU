e061f2f2d7f6465489dcbfc4c0feeba6d8d8ed87
Ezleak <116900642+Ezleak@users.noreply.github.com>
2024-07-19 11:41:11 +0800

    Update 1.sh

--- 1.sh
+++ 1.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-cd ./
-mkdir ./glslang
-cd ./glslang
-git clone --depth 1 https://github.com/KhronosGroup/glslang
+
+mkdir /glslang
+cd /glslang
+git clone --depth 1 --branch main https://github.com/KhronosGroup/glslang
 cd ..
